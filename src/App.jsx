884784e10b8f87830196cfaecd3cf7f7bc8426e6
Chathuranga-Senanayaka <senanayakacjs@gmail.com>
2025-02-17 00:33:41 +0530

    Add search component with usestate

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,10 @@
 import React from "react";
+import Search from "./components/Search";
+import { useState } from "react";
 
 function App() {
+  const [searchTerm, setSearchTerm] = useState("");
+
   return (
     <main>
       <div className="pattern" />
@@ -12,6 +16,7 @@ function App() {
             Without the Hassle
           </h1>
         </header>
+        <Search searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
       </div>
     </main>
   );
